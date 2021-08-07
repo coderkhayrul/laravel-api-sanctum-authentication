3fbcceea2f2c4d435731d414b2164989fffa0e29
Coderkhayrul <coderkhayrul>
2021-08-07 23:22:12 +0600

    Defind Controller Methord

--- app/Http/Controllers/Api/ProjectController.php
+++ app/Http/Controllers/Api/ProjectController.php
@@ -7,5 +7,23 @@ use Illuminate\Http\Request;
 
 class ProjectController extends Controller
 {
-    //
+    // CREATE PROJECT API
+    public function createProject(Request $request)
+    {
+    }
+
+    // LIST PROJECT API
+    public function listProject()
+    {
+    }
+
+    // SINGLE PROJECT API
+    public function singleProject($id)
+    {
+    }
+
+    // DELETE PROJECT API
+    public function deleteProject($id)
+    {
+    }
 }
--- app/Http/Controllers/Api/StudentController.php
+++ app/Http/Controllers/Api/StudentController.php
@@ -7,5 +7,23 @@ use Illuminate\Http\Request;
 
 class StudentController extends Controller
 {
-    //
+    // STUDENT REGISTER API
+    public function register(Request $request)
+    {
+    }
+
+    // STUDENT LOGIN API
+    public function login(Request $request)
+    {
+    }
+
+    // STUDENT PROFILE API
+    public function profile($id)
+    {
+    }
+
+    // STUDENT LOGOUT API
+    public function logout($id)
+    {
+    }
 }
