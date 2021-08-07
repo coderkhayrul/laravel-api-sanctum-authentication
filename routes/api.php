d7796d3a9acdd04ade06ea1a5a06656a3be0949f
Coderkhayrul <coderkhayrul>
2021-08-07 23:55:49 +0600

    Create API Route & Santup Route Grouping

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Http\Controllers\Api\ProjectController;
+use App\Http\Controllers\Api\StudentController;
+use App\Models\Student;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -13,6 +16,19 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+
+Route::post('/register', [StudentController::class, 'register']);
+Route::post('/login', [StudentController::class, 'login']);
+
+Route::group(['middleware' => ['auth:sanctum']], function () {
+    Route::get('profile', [StudentController::class, 'profile']);
+    Route::get('logout', [StudentController::class, 'logout']);
+
+    Route::psot('create-project', [ProjectController::class, 'createProject']);
+    Route::get('list-project', [ProjectController::class, 'listProject']);
+    Route::get('single-project/{id}', [ProjectController::class, 'singleProject']);
+    Route::delete('delete-project/{id}', [ProjectController::class, 'deleteProject']);
+});
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
